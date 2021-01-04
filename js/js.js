fb724e2d875490a617750dfbd8d5a6b8627a7907
Alena <tikalena@yandex.ru>
2021-01-04 14:15:14 +0200

    new changes
    changes 4.1

--- js/js.js
+++ js/js.js
@@ -1,12 +1,63 @@
-let lastScrollTop = 0;
-     navBar = document.getElementsByClassName("navBar");
-window.addEventListener("scroll", function(){
-    let scrollTop = window.pageYOffset || document
-        .documentElement.scrollTop;
-            if (scrollTop > lastScrollTop) {
-                navBar.style.top="-80px"
-            } else {
-                navBar.style.top="0";
-            }
-                lastScrollTop = scrollTop;
-            })
+//Navigation Bar Scroll Hide//
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navigation").style.top = "0";
+  } else {
+    document.getElementById("navigation").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+
+//My Works Graphical Section Image Changing//
+slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
+//scroll btn//
+ //Get the button
+ var mybutton = document.getElementById("myBtn");
+    
+ // When the user scrolls down 20px from the top of the document, show the button
+ window.onscroll = function() {scrollFunction()};
+ 
+ function scrollFunction() {
+   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+     mybutton.style.display = "block";
+   } else {
+     mybutton.style.display = "none";
+   }
+ }
+ 
+ // When the user clicks on the button, scroll to the top of the document
+ function topFunction() {
+   document.body.scrollTop = 0;
+   document.documentElement.scrollTop = 0;
+ }
